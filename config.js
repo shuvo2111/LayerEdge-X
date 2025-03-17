6f435c5109c9584356dc7f2dca6f55bfc1b612bd
shuvo2111 <155814909+shuvo2111@users.noreply.github.com>
2025-03-17 08:03:43 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,15 @@
 export const config = {
   baseURL: "https://referralapi.layeredge.io/api",
-  ref_code: "u9NH68wt",
-  num_ref: 500, // number of references
-  max_threads: 50, // number of threads
+  ref_code: "tLwDp8MI",
+  num_ref: 500
+  max_threads: 50
   auto_task: true,
   ////// Test key============No change
   rpc: "https://1rpc.io/base",
   //valid values: 2captcha, anticaptcha
   TYPE_CAPTCHA: "2captcha",
-  API_KEY_2CAPTCHA: "xxx", //Go to: https://2captcha.com/?from=24402314
-  API_KEY_ANTI_CAPTCHA: "xxx", //Go to: https://getcaptchasolution.com/asmsypuay9
+  API_KEY_2CAPTCHA: "86adecba22d4030d96b9b84c0c24dc96"
+  API_KEY_ANTI_CAPTCHA: "b5d612e601d59475e3fda5436c63b6dc"
   CAPTCHA_URL: "https://dashboard.layeredge.io/",
   WEBSITE_KEY: "6LdS4sEqAAAAAPCYk08sXJmhem7o0hKVxz3s0jDS",
 };
